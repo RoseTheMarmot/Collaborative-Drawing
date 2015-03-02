e2ac05ad76d64409ac8fefe174ee3ea01e779f46
RoseTheMarmot <rose.beede@gmail.com>
2015-03-02 15:45:53 -0800

    added a few comments

--- client/js/script.js
+++ client/js/script.js
@@ -1,19 +1,22 @@
 $(document).ready(function($){
   var App = function(){
+    
+    //initializing the canvas
     this.canvas = document.createElement('canvas');
     this.canvas.height = 400;
     this.canvas.width = 600;
 
+    //adding canvas to page
     $('#draw-box').append(this.canvas).children('canvas').css('width', '100%');
-    //.attr('draggable', 'true')
 
+    //initializing the drawing brush
     this.ctx = this.canvas.getContext('2d');
-
     this.ctx.fillStyle = "solid";
     this.ctx.strokeStyle = "#baba55";
     this.ctx.lineWidth = 5;
     this.ctx.lineCap = "round";
 
+    //draws lines on the canvas
     this.draw = function(x,y,type){
       if(type == "dragstart"){
         this.ctx.beginPath();
@@ -29,14 +32,13 @@ $(document).ready(function($){
 
   var drawingApp = new App();
 
-  var mousedown = false;
+  var mousedown = false; //true false if the mouse is down while being moved
   
   $("#draw-box canvas").mousedown(function(e){
     mousedown = true;
     drawingApp.draw(e.offsetX, e.offsetY, "dragstart");
   }).mousemove(function(e){
-    if(mousedown){
-      console.log(e);
+    if(mousedown){ 
       drawingApp.draw(e.offsetX, e.offsetY, "drag");
     }
   });
