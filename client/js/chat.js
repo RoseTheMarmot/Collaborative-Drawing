6b4ec3d8913336a8399b5e563ba8e06089f4d044
nikki-b <nikki.m.brown@gmail.com>
2015-03-03 10:50:44 -0800

    added anonymous chat

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
   var socket = io.connect();
   var userName = prompt("What's your name?");
+  var randNum = Math.floor(Math.random()*899)+100;
+  var addMsg = function(message){
+    $("#messages").append(message+"</br>");
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+  }
+  if(userName == "" || !userName){
+    userName = "Anonymous"+randNum;
+  };
   socket.emit("new_user", {name: userName});
   socket.on("user_accepted", function(data){
     for(var i=0; i<data.chats.length; i++){
-      $("#messages").prepend(data.chats[i]+"</br>");
+      $("#messages").append(data.chats[i]+"</br>");
     };
-    $("#messages").prepend(data.name+" entered the chatroom!</br>")
+    $("#messages").scrollTop($("#messages")[0].scrollHeight);
+    
+  });
+  socket.on("user_entered", function(data){
+    addMsg(data.chat);
   });
   $("#send_btn").click(function(e){
     e.preventDefault();
@@ -20,9 +32,9 @@ $(document).ready(function(){
     };
   });
   socket.on("msg_received", function(data){
-    $("#messages").prepend(data.message+"</br>");
+    addMsg(data.chat);
   });
   socket.on("user_disconnected", function(data){
-    $("#messages").prepend(data.name+" left the chatroom.</br>");
+    addMsg(data.chat);
   }); 
 });
