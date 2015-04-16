b852d52e6bd21352b59956c3f6f833075a113e97
RoseTheMarmot <rose.beede@gmail.com>
2015-04-16 10:34:48 -0700

    edited gitignore

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,11 +3,12 @@
  */
 
 var mongoose = 	require('mongoose');
-var fs 		 = 	require('fs');
+var fs 		   = 	require('fs');
+var keys     =  require(__dirname+'/keys.js')
 
 //connect to mongodb server
-//mongoose.connect('mongodb://localhost/CollaborativeDrawing');
-mongoose.connect('mongodb://heroku_app35932629:59r4l84ta376l6l1c112hs32v@ds061631.mongolab.com:61631/heroku_app35932629?replicaSet=rs-ds061631');
+mongoose.connect(keys.local_db);
+//mongoose.connect('mongodb://heroku_app35932629:59r4l84ta376l6l1c112hs32v@ds061631.mongolab.com:61631/heroku_app35932629?replicaSet=rs-ds061631');
 
 //require all .js files in the modles directory
 var models_path = __dirname + "/../models";
