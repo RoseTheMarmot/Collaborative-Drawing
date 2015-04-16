7c178bb67739e0f444fe386abb63fef38ca2310e
RoseTheMarmot <rose.beede@gmail.com>
2015-04-16 10:41:13 -0700

    more gitignore edits

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+/*
+ * Passwords and other senitive stuff
+ */
+
+module.exports = {
+	local_db:'mongodb://localhost/CollaborativeDrawing',
+	remote_db:'mongodb://heroku_app35932629:59r4l84ta376l6l1c112hs32v@ds061631.mongolab.com:61631/heroku_app35932629?replicaSet=rs-ds061631'
+}
