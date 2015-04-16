d426138d57c0f8a01cbb081ef6bfada73d19a1c2
RoseTheMarmot <rose.beede@gmail.com>
2015-04-16 10:36:31 -0700

    editing mongoose

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -4,11 +4,10 @@
 
 var mongoose = 	require('mongoose');
 var fs 		   = 	require('fs');
-var keys     =  require(__dirname+'/keys.js')
+var keys     =  require(__dirname+'/keys.js');
 
 //connect to mongodb server
-mongoose.connect(keys.local_db);
-//mongoose.connect('mongodb://heroku_app35932629:59r4l84ta376l6l1c112hs32v@ds061631.mongolab.com:61631/heroku_app35932629?replicaSet=rs-ds061631');
+mongoose.connect(keys.remote_db);
 
 //require all .js files in the modles directory
 var models_path = __dirname + "/../models";
