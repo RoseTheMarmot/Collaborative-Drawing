c8a9d09ab106ac316a886e7400b468d07e2ffa37
nikki-b <nikki.m.brown@gmail.com>
2015-07-10 14:13:40 -0700

    db updates

--- server/config/keys.js
+++ server/config/keys.js
@@ -4,5 +4,5 @@
 
 module.exports = {
 	local_db:'mongodb://localhost/CollaborativeDrawing',
-	remote_db:'mongodb://admin:admin@ds059661.mongolab.com:59661/heroku_app35234027'
+	remote_db:'mongodb://admin:admin@ds047752.mongolab.com:47752/heroku_f6cqgzv3'
 }
