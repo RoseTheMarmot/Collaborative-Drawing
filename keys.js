ba5648f26f6673d0a926b46d80911375cb7dc864
nikki-b <nikki.m.brown@gmail.com>
2015-07-10 14:02:58 -0700

    first commit

--- keys.js
+++
@@ -1,8 +0,0 @@
-/*
- * Passwords and other sensitive stuff
- */
-
-module.exports = {
-	local_db:'mongodb://localhost/CollaborativeDrawing',
-	remote_db:'mongodb://admin:admin@ds059661.mongolab.com:59661/heroku_app35234027'
-}
