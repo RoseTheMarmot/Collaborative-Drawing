fcb5b243770727d7842c8132a9eb5fc2b9bf495f
nikki-b <nikki.m.brown@gmail.com>
2015-10-01 11:51:23 -0700

    removed keys

--- server/config/keys.js
+++
@@ -1,8 +0,0 @@
-/*
- * Passwords and other sensitive stuff
- */
-
-module.exports = {
-	local_db:'mongodb://localhost/CollaborativeDrawing',
-	remote_db:'mongodb://admin:admin@ds047752.mongolab.com:47752/heroku_f6cqgzv3'
-}
